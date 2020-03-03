228007d96b10eb7647e5d07145650f06edbfc405
godofevil <rkolodiazhniy@odessa.gov.ua>
2020-03-03 12:39:50 +0200

    first commit

---
+++ precache-manifest.0fc4897b16687fcb16cba81e358ed962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f768227a96323549fbe02617e7f94c72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17912deb565614ad38d0",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a8722f7410e5ca9111ea",
+    "url": "/static/css/main.67fb5835.chunk.css"
+  },
+  {
+    "revision": "17912deb565614ad38d0",
+    "url": "/static/js/2.c5c3fc04.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c5c3fc04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8722f7410e5ca9111ea",
+    "url": "/static/js/main.09193724.chunk.js"
+  },
+  {
+    "revision": "64fcfbb63c8058e2c0b8",
+    "url": "/static/js/runtime-main.253dcd42.js"
+  }
+]);
