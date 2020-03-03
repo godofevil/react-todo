d5ae12350fc5a912ed3094e039b5eaa3f3e83288
godofevil <rkolodiazhniy@odessa.gov.ua>
2020-03-03 15:24:13 +0200

    fixes

---
+++ build/precache-manifest.18b72001c8a9a91904f9c45bb4e9c23f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a03ff0331cd5248f23d19abb8830b9",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "17912deb565614ad38d0",
+    "url": "/react-todo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "613b2213071908a8ef2f",
+    "url": "/react-todo/static/css/main.8a0c9f2f.chunk.css"
+  },
+  {
+    "revision": "17912deb565614ad38d0",
+    "url": "/react-todo/static/js/2.c5c3fc04.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-todo/static/js/2.c5c3fc04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613b2213071908a8ef2f",
+    "url": "/react-todo/static/js/main.ae977ffc.chunk.js"
+  },
+  {
+    "revision": "a1771700c5b5be6532b7",
+    "url": "/react-todo/static/js/runtime-main.f550537c.js"
+  }
+]);
